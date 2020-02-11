553278ec1b4833da6326dbff03301843157046e0
kobrata0912 <40165358+kobrata0912@users.noreply.github.com>
2020-02-11 13:18:54 +0200

    About page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NewsComponent } from './news/news/news.component';
+import { ConfiguratorComponent } from './configurator/configurator/configurator.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 
 const routes: Routes = [
@@ -25,6 +30,31 @@ const routes: Routes = [
     path: 'register',
     pathMatch: 'full',
     component: RegisterComponent
+  },
+  {
+    path: 'aboutus',
+    pathMatch: 'full',
+    component: AboutUsComponent
+  },
+  {
+    path: 'contacts',
+    pathMatch: 'full',
+    component: ContactsComponent
+  },
+  {
+    path: 'news',
+    pathMatch: 'full',
+    component: NewsComponent
+  },
+  {
+    path: 'configurator',
+    pathMatch: 'full',
+    component: ConfiguratorComponent
+  },
+  {
+    path: 'profile',
+    pathMatch: 'full',
+    component: ProfileComponent
   }
 ];
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,8 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [
-    './register.component.scss',
-    '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
+    './register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
