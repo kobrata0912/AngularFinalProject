f5dbe9dc875749e6cdbf4f3b1fd8381c1cd7fe4f
kobrata0912 <40165358+kobrata0912@users.noreply.github.com>
2020-02-12 13:16:31 +0200

    Adding auth services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user/user.service';
+import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
-import { FirebaseService } from '../firebase.service';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
 	selector: 'app-login',
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user.service';
+import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { passwordMatch } from 'src/app/shared/validators/password-match';
